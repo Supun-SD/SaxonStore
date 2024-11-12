59399ed2eb2eb73179809a5a03d63de17e9b9503
supun <supundunusingha2@gmail.com>
2024-11-12 21:10:49 +0530

    fixes

--- saxonstore-frontend/src/pages/ProductManagement.jsx
+++ saxonstore-frontend/src/pages/ProductManagement.jsx
@@ -1,5 +1,6 @@
-import React, { useState, useEffect, useRef } from 'react';
-import '@fortawesome/fontawesome-free/css/all.min.css';
+import { useState, useEffect, useRef } from "react";
+import MyProduct from "../components/MyProduct";
+import Button from "../components/Button";
 
 const ProductManagement = () => {
   const [isMenOpen, setIsMenOpen] = useState(false);
@@ -30,7 +31,9 @@ const ProductManagement = () => {
   ];
 
   const handleDeleteClick = (productName) => {
-    const confirmed = window.confirm(`Are you sure you want to delete ${productName}? This action cannot be undone, and all inventory data for this item will be permanently removed.`);
+    const confirmed = window.confirm(
+      `Are you sure you want to delete ${productName}? This action cannot be undone, and all inventory data for this item will be permanently removed.`,
+    );
     if (confirmed) {
       alert(`${productName} has been deleted.`);
     }
@@ -53,87 +56,78 @@ const ProductManagement = () => {
   }, []);
 
   return (
-    <div className="flex flex-col h-screen justify-center items-center pt-16">
-      <div className="pt-32 p-6 md:p-10 lg:p-12 space-y-8 w-full max-w-7xl">
-        {/* Heading */}
-        <h1 className="text-3xl font-bold mb-4 text-center md:text-left">My Products</h1>
+    <div className="mx-6 mb-20 mt-32 w-full max-w-7xl flex-col lg:mx-auto">
+      {/* Heading */}
+      <h1 className="mb-6 text-start font-serif text-3xl font-bold">
+        My Products
+      </h1>
 
-        {/* Search and Filter */}
-        <div className="flex flex-col md:flex-row items-center space-y-4 md:space-y-0 md:space-x-4 w-full">
-          <input
-            type="text"
-            placeholder="Search for product by name or product number"
-            className="flex-grow p-3 border border-gray-300 w-full md:w-1/3 bg-gray-100 rounded-none"
-          />
+      {/* Search and Filter */}
+      <div className="mb-8 flex w-full flex-col items-center md:flex-row md:space-x-4 md:space-y-0">
+        <input
+          type="text"
+          placeholder="Search for product by name or product number"
+          className="w-full flex-grow rounded-none border border-gray-300 bg-gray-100 p-3 md:w-1/3"
+        />
 
-          <div className="relative w-full md:w-auto" ref={dropdownRef}>
-            <button
-              className="p-3 border border-gray-300 w-full md:w-56 bg-gray-100 text-left rounded-none flex justify-between items-center"
-              onClick={() => setIsDropdownOpen(!isDropdownOpen)}
-            >
-              All Products
-              <i className="fas fa-chevron-down ml-2 text-gray-500"></i>
-            </button>
-
-            {isDropdownOpen && (
-              <div className="absolute left-0 mt-2 w-full md:w-56 bg-white border border-gray-300 rounded-md shadow-lg">
-                {categories.map((category, index) => (
-                  <div key={index}>
-                    <button
-                      className="block w-full text-left px-4 py-2 hover:bg-gray-100 flex justify-between items-center"
-                      onClick={() => {
-                        if (category.name === "Men's Wear") setIsMenOpen(!isMenOpen);
-                        else if (category.name === "Women's Wear") setIsWomenOpen(!isWomenOpen);
-                      }}
-                    >
-                      {category.name}
-                      <i className="fas fa-chevron-right ml-4 text-gray-500"></i>
-                    </button>
-                    {(category.name === "Men's Wear" && isMenOpen) ||
-                    (category.name === "Women's Wear" && isWomenOpen) ? (
-                      <div className="pl-6">
-                        {category.subcategories.map((subcategory, subIndex) => (
-                          <button key={subIndex} className="block w-full text-left px-4 py-2">
-                            {subcategory}
-                          </button>
-                        ))}
-                      </div>
-                    ) : null}
-                  </div>
-                ))}
-              </div>
-            )}
-          </div>
-
-          <button className="border border-black text-black px-6 py-3 w-full md:w-auto rounded-none">
-            ADD PRODUCT
+        <div className="relative w-full md:w-auto" ref={dropdownRef}>
+          <button
+            className="flex w-full items-center justify-between rounded-none border border-gray-300 bg-gray-100 p-3 text-left md:w-56"
+            onClick={() => setIsDropdownOpen(!isDropdownOpen)}
+          >
+            All Products
+            <i className="fas fa-chevron-down ml-2 text-gray-500"></i>
           </button>
+
+          {isDropdownOpen && (
+            <div className="absolute left-0 mt-2 w-full rounded-md border border-gray-300 bg-white shadow-lg md:w-56">
+              {categories.map((category, index) => (
+                <div key={index}>
+                  <button
+                    className="flex w-full items-center justify-between px-4 py-2 text-left hover:bg-gray-100"
+                    onClick={() => {
+                      if (category.name === "Men's Wear")
+                        setIsMenOpen(!isMenOpen);
+                      else if (category.name === "Women's Wear")
+                        setIsWomenOpen(!isWomenOpen);
+                    }}
+                  >
+                    {category.name}
+                    <i className="fas fa-chevron-right ml-4 text-gray-500"></i>
+                  </button>
+                  {(category.name === "Men's Wear" && isMenOpen) ||
+                  (category.name === "Women's Wear" && isWomenOpen) ? (
+                    <div className="pl-6">
+                      {category.subcategories.map((subcategory, subIndex) => (
+                        <button
+                          key={subIndex}
+                          className="block w-full px-4 py-2 text-left"
+                        >
+                          {subcategory}
+                        </button>
+                      ))}
+                    </div>
+                  ) : null}
+                </div>
+              ))}
+            </div>
+          )}
         </div>
 
-        {/* Product List */}
-        <div className="grid gap-8 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-4 w-full">
-          {products.map((product, index) => (
-            <div key={index} className="p-4 border border-gray-200 rounded-lg hover:shadow-lg transition-shadow">
-              <div className="bg-gray-200 h-40 mb-4 rounded-md"></div>
-              <h2 className="text-lg font-semibold">{product.name}</h2>
-              <p className="text-gray-500">{product.id}</p>
-              <div className="mt-4 flex flex-col md:flex-row md:justify-between space-y-2 md:space-y-0 md:space-x-2">
-                <button className="text-black border border-gray-400 px-4 py-2 rounded-none">
-                  UPDATE
-                </button>
-                <button className="text-black border border-gray-400 px-4 py-2 rounded-none">
-                  <i className="fas fa-eye"></i>
-                </button>
-                <button
-                  onClick={() => handleDeleteClick(product.name)}
-                  className="bg-black text-white px-4 py-2 rounded-none"
-                >
-                  <i className="fas fa-trash"></i>
-                </button>
-              </div>
-            </div>
-          ))}
-        </div>
+        <button className="h-full border border-black px-8 py-3 text-black transition duration-300 hover:bg-black hover:text-white">
+          ADD PRODUCT
+        </button>
+      </div>
+
+      {/* Product List */}
+      <div className="grid w-full gap-8 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-4">
+        {products.map((product, index) => (
+          <MyProduct
+            product={product}
+            key={index}
+            handleDeleteClick={handleDeleteClick}
+          />
+        ))}
       </div>
     </div>
   );
