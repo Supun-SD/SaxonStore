d2d26ac6e40a6a1c5bcc3824dbfb04c726f605e8
Supun D <supundunusingha2@gmail.com>
2024-11-21 21:57:43 +0530

    Update ProductCard.jsx

--- saxonstore-frontend/src/components/ProductCard.jsx
+++ saxonstore-frontend/src/components/ProductCard.jsx
@@ -17,13 +17,14 @@ function ProductCard({ product }) {
       onClick={handleClick}
     >
       <div className="w-full flex-col">
-        <div className="w-full overflow-hidden">
+        <div className="h-64 w-full overflow-hidden">
           <img
             src={primaryImage}
             alt="product image"
-            className="h-auto w-full object-cover transition-transform duration-300 ease-in-out hover:scale-110"
+            className="h-full w-full object-cover transition-transform duration-300 ease-in-out hover:scale-110"
           />
         </div>
+
         <div className="mt-3 text-lg">{product.name}</div>
         <div className="mt-1">LKR {product.price.toFixed(2)}</div>
       </div>
